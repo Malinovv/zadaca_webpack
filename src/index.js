ffb5a01bcdbe6108d35b2d071e38145b7a26510e
Kristijan Malinov <kristijan.malinovv@gmail.com>
2023-12-08 03:10:54 +0100

    combining carousel and todo, with implementation of splidejs

--- src/index.js
+++ src/index.js
@@ -1,5 +1,15 @@
-import { init as carouselInit } from "./js/carousel.js";
+import { toDoInit } from "./js/todo.js";
+import Splide from "../node_modules/@splidejs/splide/dist/js/splide.js";
+
+new Splide(".splide", {
+  type: "loop",
+  perPage: 1,
+  rewind: true,
+  rewindByDrag: true,
+  heightRatio: 0.3,
+  speed: 1200,
+}).mount();
 
 console.log("Start");
 
-carouselInit();
+toDoInit();
